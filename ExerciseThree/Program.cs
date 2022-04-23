c93799c42758fc117033e2885b9478563726e58b
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-24 08:00:31 +1100

    Add Exercise Three

---
+++ ExerciseThree/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int i = 0;
+
+while(i < N)
+{
+    i++;
+    count = i*i;
+    Console.WriteLine(count);
+}
