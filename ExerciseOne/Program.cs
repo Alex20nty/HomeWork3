91c0df0c6555b8496485e5de7ba2a7aa0546c072
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-24 00:48:49 +1100

    Add Exercise One

---
+++ ExerciseOne/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите х: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите у: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if((x > 0) && (y > 0))
+Console.WriteLine("1-я четверть ");
+else
+{
+    if((x < 0) && (y > 0))
+    Console.WriteLine("2-я четверть ");
+    else
+    {
+        if((x < 0) && (y < 0))
+        Console.WriteLine("3-я четверть ");
+        else
+        {
+            if((x > 0) && (y < 0))
+            Console.WriteLine("4-я четверть ");
+            else
+            {
+                if((x!=0) && (y!=0))
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+            
+        
+    
+
+        
