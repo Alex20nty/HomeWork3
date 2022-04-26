0657cffa44fb7386429764e4fc2842e25d59a01a
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-27 03:07:06 +1100

    Add Exercises Two and Four

---
+++ ExerciseFour/Program.cs
@@ -0,0 +1,31 @@
+﻿int GetValue(string num)
+{
+    System.Console.WriteLine($"{num}: ");
+    string getValueStr = Console.ReadLine()!;
+    int value = Convert.ToInt32(getValueStr);
+    return value;
+}
+
+int ReverseNumber(int numA)
+{
+    int a = 0;
+    while(numA > 0)
+    {
+        a = a * 10 + numA % 10;
+        numA = numA / 10;
+    }  
+    return a;      
+}
+
+int Num = GetValue("Номерочек");
+int result = ReverseNumber(Num);
+System.Console.WriteLine(result);
+
+if(result == Num)
+    {
+        Console.WriteLine("Это полиндром");
+    }
+        else
+            {
+                System.Console.WriteLine("Это не полиндром");
+            }
---
+++ ExerciseTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetValue(string text)
+{
+    System.Console.WriteLine($"{text}: ");
+    string getValueStr = Console.ReadLine();
+    int value = Convert.ToInt32(getValueStr);
+    return value;   
+}
+
+double Pifagor(int ax, int ay, int bx, int by)
+{
+    return Math.Sqrt((bx - ax)*(bx - ax) + (by - ay)*(by - ay));
+}
+
+int Ax = GetValue("Ax");
+int Ay = GetValue("Ay");
+int Bx = GetValue("Bx");
+int By = GetValue("By");
+double result = Pifagor(Ax, Ay, Bx, By);
+System.Console.WriteLine(result);
