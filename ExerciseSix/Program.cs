28f26af7a52ff59766a0bf6a3a935ae1536a4a0f
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-27 07:13:26 +1100

    Add Exercise Six

---
+++ ExerciseSix/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int i = 0;
+
+while(i < N)
+{
+    i++;
+    count = Convert.ToInt32(Math.Pow(i, 3));
+    Console.Write($" {count} ");
+}
+
